1730c7a7454cc413915b6b1e56bed322e38fd3da
batzionL <batziona99@gmail.com>
2025-11-24 13:59:54 +0200

    first commit

---
+++ client/js/add_moderator.js
@@ -0,0 +1,94 @@
+jQuery(function ($) {
+    const random7DigitNumber = getRandom7DigitNumber();
+    document.getElementById("mod_pswd_id").value = random7DigitNumber;
+})
+
+function getRandom7DigitNumber() {
+    const min = 100000;
+    const max = 999999;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function add_mod() {
+    var id = document.getElementById("id_mod").value;
+    if (id.length != 9 || !checkId(id)) {
+        alert("מס' ת.ז. לא תקין")
+    }
+    else {
+        checkIfModExist(document.getElementById('id_mod').value);
+        var flag = localStorage.getItem('flag')
+        if (flag === 'false') {
+            $.ajax({
+                type: 'POST', 
+                url: '/addmoderator', 
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    "username": $("#mod_username_id").val(),
+                    "password": $("#mod_pswd_id").val(),
+                    "mod_firstName": $("#firstName_mod_id").val(),
+                    "mod_lastName": $("#lastName_mod_id").val(),
+                    "mod_ID": $("#id_mod").val(),
+                    "mod_email": $("#email_mod_id").val()
+                }),
+                processData: false,
+                encode: true,
+                success: function (data, textStatus, jQxhr) {
+                    location.href = "/assigAndsubDats"
+                },
+                error: function (jqXhr, textStatus, errorThrown) {
+                    console.log(errorThrown);
+                }
+            });
+        }
+    }
+}
+
+function checkIfModExist(id) {
+    $.ajax({
+        type: 'GET',
+        url: '/moderator/' + id,
+        success: function (result) {
+            if (result[0] != undefined) {
+                alert('מנחה זה כבר קיים');
+                localStorage.setItem('flag', "true");
+                location.reload();
+            }
+            else {
+                localStorage.setItem('flag', 'false')
+            }
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    })
+}
+
+function add_coor(){
+    location.href = "/addcoordinator"
+}
+// 
+function checkId(id) {
+    let checkArray = [];
+    let checkNum = 0, sum = 0;
+    
+    for (let i = 0; i < id.length - 1; i++) {
+        if (i % 2 === 0) {
+            checkNum = id[i] * 2;
+            if (checkNum > 9)
+                checkNum = checkNum / 10 + checkNum % 10;
+            checkArray.push(checkNum); 
+        } else {
+            checkArray.push(id[i]); 
+        }
+    }
+    for(let num of checkArray){
+        sum += num;
+    }
+    checkNum = sum + id[id.length-1];
+    if(checkNum === 30){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
---
+++ client/js/login.js
@@ -0,0 +1,258 @@
+// ====================== כניסה ======================
+async function sub(e) {
+    e.preventDefault();
+
+    const username = document.getElementById("id_username").value.trim();
+    const password = document.getElementById("id_password").value;
+
+    if (!username) {
+        alert("אנא הזיני שם משתמש");
+        return;
+    }
+
+    // בדיקה אם המשתמש קיים כסטודנט
+    const student = await getStudentInfo(username);
+    if (student) {
+        if (!password) {
+            await sendEmail(student.sdt_email, student.password.trim());
+            alert("סיסמה נשלחה למייל שלך");
+            return;
+        }
+        if (password.trim() === student.password.trim()) {
+            localStorage.setItem("data", "student");
+            localStorage.setItem("name", student.sdt_firstName + " " + student.sdt_lastName);
+            localStorage.setItem("stdID", student.sdt_ID);
+            localStorage.setItem("modID", "");
+            window.location.href = "/assigAndsubDats";
+            return;
+        } else {
+            alert("סיסמה שגויה לסטודנט");
+            return;
+        }
+    }
+
+    // בדיקה אם המשתמש קיים כמודרטור
+    const mod = await getModeratorInfo(username);
+    if (mod) {
+        if (!password) {
+            await sendEmail(mod.mod_email, mod.password.trim());
+            alert("סיסמה נשלחה למייל שלך");
+            return;
+        }
+        if (password.trim() === mod.password.trim()) {
+            const name = mod.mod_firstName + " " + mod.mod_lastName;
+            await is_coor(mod.mod_ID, name);
+            return;
+        } else {
+            alert("סיסמה שגויה למודרטור");
+            return;
+        }
+    }
+
+    // אם לא נמצא סטודנט ולא מודרטור
+    alert("שם משתמש לא קיים במערכת");
+}
+
+// ====================== קבלת סטודנט ======================
+async function getStudentInfo(username) {
+    try {
+        const result = await $.ajax({ type: 'GET', url: '/getStudentPwd/' + username });
+        if (result[0]) return result[0];
+        return null;
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
+}
+
+// ====================== קבלת מודרטור ======================
+async function getModeratorInfo(username) {
+    try {
+        const result = await $.ajax({ type: 'GET', url: '/getModeratorPwd/' + username });
+        if (result[0]) return result[0];
+        return null;
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
+}
+
+// ====================== בדיקת קואורדינטור ======================
+async function is_coor(id, name) {
+    try {
+        const result = await $.ajax({ type: 'GET', url: '/getCoodinator' });
+        if (!result[0] || result[0].coo_ID !== id) {
+            localStorage.setItem("data", "moderator");
+        } else {
+            localStorage.setItem("data", "coordinator");
+        }
+        localStorage.setItem("name", name);
+        localStorage.setItem("modID", id);
+        localStorage.setItem("stdID", "");
+        window.location.href = "/assigAndsubDats";
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// ====================== שליחת סיסמה במייל ======================
+function sendEmail(userEmail, password) {
+    return $.ajax({
+        type: 'POST',
+        url: '/sendEmail',
+        contentType: 'application/json',
+        data: JSON.stringify({ email: userEmail, password: password }),
+    }).done(() => console.log('סיסמה נשלחה במייל'))
+      .fail((jqXhr, textStatus, errorThrown) => console.error(errorThrown));
+}
+
+
+
+// function sub(e) {
+//     e.preventDefault()
+//     var username = document.getElementById("id_username").value;
+//     var password = document.getElementById("id_password").value;
+//     getStudentUsername(username, password);
+//     getModeratorUsername(username, password);
+// }
+
+
+// // Adjusting is_coor to async
+// function is_coor(id, name) {
+//     $.ajax({
+//         type: 'GET',
+//         url: '/getCoodinator',
+//         success: function (result) {
+//             if (result[0].coo_ID === undefined || result[0].coo_ID != id) {
+//                 localStorage.setItem("data", "moderator");
+//             } else {
+//                 localStorage.setItem("data", "coordinator");
+//             }
+//             localStorage.setItem("name", name);
+//             localStorage.setItem("modID", id);
+//             localStorage.setItem("stdID", "");
+//             window.location.href = "/assigAndsubDats";
+//         },
+//         error: function (jqXhr, textStatus, errorThrown) {
+//             console.log(errorThrown);
+//         }
+//     });
+// }
+
+// function showPassword(str) {
+//     var x;
+//     switch (str) {
+//         case 'sdt':
+//             x = document.getElementById("sdt_pswd_id");
+//             break;
+//         case 'mod':
+//             x = document.getElementById('mod_pswd_id');
+//             break;
+//         case 'old':
+//             x = document.getElementById('old_pwd_or_ID');
+//             break;
+//         case 'new':
+//             x = document.getElementById('id_new_pwd');
+//             break;
+//         case 'again':
+//             x = document.getElementById('id_again_new_pwd');
+//             break;
+//         case 'login':
+//             x = document.getElementById('id_password');
+//             break;
+//     }
+
+//     x.type = x.type === 'password' ? 'text' : 'password';
+// }
+
+// function resetPassword() {
+//     var username = document.getElementById("id_username").value;
+//     localStorage.setItem('username', username);
+//     window.location.href = "/changepassword";
+// }
+
+// function firsTime() {
+//     var username = document.getElementById("id_username").value;
+//     var password = document.getElementById("id_password").value;
+
+//     getStudentUsername(username, password);
+//     getModeratorUsername(username, password);
+// }
+
+// function sendEmail(userEmail, password) {
+//     $.ajax({
+//         type: 'POST',
+//         url: '/sendEmail',
+//         contentType: 'application/json',
+//         data: JSON.stringify({
+//             "email": userEmail,
+//             "password": password
+//         }),
+//         processData: false,
+//         encode: true,
+//         success: function (data, textStatus, jQxhr) {
+//             alert('הסיסמה לכניסה ראשונה נשלחה לך במייל');
+//         },
+//         error: function (jqXhr, textStatus, errorThrown) {
+//             console.log(errorThrown);
+//         }
+//     });
+// }
+
+// function getStudentUsername(username, sdtPwd) {
+//     $.ajax({
+//         type: 'GET',
+//         url: '/getStudentPwd/' + username,
+//         success: function (result) {
+//             if (result[0] != undefined) {
+//                 if (sdtPwd == "") {
+//                     sendEmail(result[0].sdt_email, result[0].password)
+//                 } else if (sdtPwd.length >= 8 && sdtPwd.length <= 14) {
+//                     if (sdtPwd == result[0].password) {
+//                         localStorage.setItem("data", "student");
+//                         var name = result[0].sdt_firstName + " " + result[0].sdt_lastName;
+//                         localStorage.setItem("name", name);
+//                         localStorage.setItem("stdID", result[0].sdt_ID);
+//                         localStorage.setItem("modID", "");
+//                         window.location.href = "/assigAndsubDats";
+//                     }
+//                 } else if (sdtPwd.length == 6) {
+//                     if (sdtPwd == result[0].password) {
+//                         resetPassword();
+//                     }
+//                 }
+//             }
+//         },
+//         error: function (jqXhr, textStatus, errorThrown) {
+//             console.log(errorThrown);
+//         }
+//     });
+
+// }
+
+// function getModeratorUsername(username, modPwd) {
+//     $.ajax({
+//         type: 'GET',
+//         url: '/getModeratorPwd/' + username,
+//         success: function (result) {
+//             if (result[0] != undefined) {
+//                 if (modPwd == "") {
+//                     sendEmail(result[0].mod_email, result[0].password)
+//                 } else if (modPwd.length >= 8 && modPwd.length <= 14) {
+//                     if (modPwd == result[0].password) {
+//                         var name = result[0].mod_firstName + " " + result[0].mod_lastName;
+//                         is_coor(result[0].mod_ID, name);
+//                     }
+//                 } else if (modPwd.length == 6) {
+//                     if (modPwd == result[0].password) {
+//                         resetPassword();
+//                     }
+//                 }
+//             }
+//         },
+//         error: function (jqXhr, textStatus, errorThrown) {
+//             console.log(errorThrown);
+//         }
+//     });
+
+// }
